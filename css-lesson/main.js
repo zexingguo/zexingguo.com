05dc324272fdf45722f2ff9ae54182e64e1aa70b
Jack G <zexingguo@gmail.com>
2017-03-25 11:05:49 -0700

    new changes

--- css-lesson/main.js
+++ css-lesson/main.js
@@ -16,7 +16,7 @@ $( document ).ready(function() {
 
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Mar 25, 2017 11:00:00").getTime();
+    var countDownDate = new Date("Mar 25, 2017 11:15:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
