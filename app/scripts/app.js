d34ea26efb370ba0107369b9fe60e59a5e47761e
zexing jack guo <zexingguo@gmail.com>
2017-10-20 00:05:14 -0700

    added favicon, finished all sections, just need more polish on fancy stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,47 @@
-//Feather Icon
-feather.replace()
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  //Feather Icon
+  feather.replace();
+
+  
+  
+});
+
+function animateCircle() {
+  //Auto animation for progress circle
+  var circles = document.querySelectorAll('.progress-circle');
+  console.log(circles);
+  var start = 1;
+  var i = 0;
+
+  setInterval(function () {
+    if (i < circles.length) {
+      var circle = circles[i]
+      var end = circles[i].getAttribute("data-progress")
+      circles[i].setAttribute("data-progress", start)
+
+      test(end, circle, i)
+    }
+
+    else return
+    i++
+
+  }, 100)
+
+  function test(end, circle) {
+    var a = 0
+    var startLoad = setInterval(function () {
+      if (a >= end) {
+        // console.log('loop end')
+        // console.log('circle: ', circle)
+        clearInterval(startLoad)
+      } else {
+        a++
+        circle.setAttribute("data-progress", a)
+      }
+    }, 20, end, circle)
+  }
+}
 /* Modernizr 2.8.3 (Custom Build) | MIT & BSD
  * Build: http://modernizr.com/download/#-fontface-backgroundsize-borderimage-borderradius-boxshadow-flexbox-hsla-multiplebgs-opacity-rgba-textshadow-cssanimations-csscolumns-generatedcontent-cssgradients-cssreflections-csstransforms-csstransforms3d-csstransitions-applicationcache-canvas-canvastext-draganddrop-hashchange-history-audio-video-indexeddb-input-inputtypes-localstorage-postmessage-sessionstorage-websockets-websqldatabase-webworkers-geolocation-inlinesvg-smil-svg-svgclippaths-touch-webgl-shiv-mq-cssclasses-addtest-prefixed-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-load
  */
--- app/scripts/src/_includes/main.js
+++ app/scripts/src/_includes/main.js
@@ -1,2 +1,44 @@
-//Feather Icon
-feather.replace()
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  //Feather Icon
+  feather.replace();
+
+  
+  
+});
+
+function animateCircle() {
+  //Auto animation for progress circle
+  var circles = document.querySelectorAll('.progress-circle');
+  console.log(circles);
+  var start = 1;
+  var i = 0;
+
+  setInterval(function () {
+    if (i < circles.length) {
+      var circle = circles[i]
+      var end = circles[i].getAttribute("data-progress")
+      circles[i].setAttribute("data-progress", start)
+
+      test(end, circle, i)
+    }
+
+    else return
+    i++
+
+  }, 100)
+
+  function test(end, circle) {
+    var a = 0
+    var startLoad = setInterval(function () {
+      if (a >= end) {
+        // console.log('loop end')
+        // console.log('circle: ', circle)
+        clearInterval(startLoad)
+      } else {
+        a++
+        circle.setAttribute("data-progress", a)
+      }
+    }, 20, end, circle)
+  }
+}
