7133b35d130e5c8abda424133ad05379a4b714be
zexing jack guo <zexingguo@gmail.com>
2018-05-02 20:40:58 -0700

    update lock file

--- app/scripts/src/_includes/main.js
+++ app/scripts/src/_includes/main.js
@@ -21,71 +21,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 });
 
 
-// Listen to Scroll and animate Circle
-// window.addEventListener('scroll', function (e) {
-  
-//   var skills = document.querySelector('.home__skills-more');
-//   let firstAppear = false;
-
-//   if (isInViewport(skills)) {
-//     if (!firstAppear) {
-//       console.log('always herer', firstAppear)
-//       // animateCircle();
-//       firstAppear = true;
-//     } else {
-//       firstAppear = true
-//     }
-//   }
-
-//   function isInViewport(element) {
-//     var rect = element.getBoundingClientRect();
-//     var html = document.documentElement;
-//     return (
-//       rect.top >= 0 &&
-//       rect.left >= 0 &&
-//       rect.bottom <= (window.innerHeight || html.clientHeight) &&
-//       rect.right <= (window.innerWidth || html.clientWidth)
-//     );
-//   }
-
-// });
-
-// function animateCircle() {
-//   //Auto animation for progress circle
-//   var circles = document.querySelectorAll('.progress-circle');
-//   console.log(circles);
-//   var start = 1;
-//   var i = 0;
-
-//   setInterval(function () {
-//     if (i < circles.length) {
-//       var circle = circles[i]
-//       var end = circles[i].getAttribute("data-progress")
-//       circles[i].setAttribute("data-progress", start)
-
-//       startLoad(end, circle, i)
-//     }
-
-//     else return
-//     i++
-
-//   }, 100)
-
-//   function startLoad(end, circle) {
-//     var a = 0
-//     var startLoad = setInterval(function () {
-//       if (a >= end) {
-//         // console.log('loop end')
-//         // console.log('circle: ', circle)
-//         clearInterval(startLoad)
-//       } else {
-//         a++
-//         circle.setAttribute("data-progress", a)
-//       }
-//     }, 10, end, circle)
-//   }
-// }
-
 // Smooth Scroll in Plain Javascript
 window.MobilesmoothScroll = function (target) {
   var mobile_nav = document.querySelector('.mobile-nav');
@@ -142,9 +77,4 @@ window.smoothScroll = function (target) {
   }
   // start scrolling
   scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-}
-
-//Send Email
-function sendMail() {
-  
 }
